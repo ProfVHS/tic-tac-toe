448024d58a43d4544d4c7400178d0ba9db0ff1bd
ProfVHS <rubajrafal04@gmail.com>
2022-11-25 15:35:30 +0100

    main menu style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Link to="/singleplayer">Singleplayer</Link>
-      <Link to="/multiplayer">Multiplayer</Link>
-    </>
+    <div className="wrapper">
+      <div className="card">
+        <span className="card__title">Tic Tac Toe</span>
+        <Link to="/singleplayer"><button className="card__button">Singleplayer</button></Link>
+        <Link to="/multiplayer"><button className="card__button">Multiplayer</button></Link>
+      </div>
+    </div>
   );
 };
 
