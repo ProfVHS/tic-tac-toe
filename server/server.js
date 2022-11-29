9c946c59c27d2d64bee54e68b03e65251c5a9e5f
devdb <rubajrafal04@gmail.com>
2022-11-29 14:54:18 +0100

    Origin change

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://tic-tac-toe-2dtb.onrender.com",
         methods: ["GET", "POST"],
     }
 });
